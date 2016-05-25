584f013ede2c68237045f1686b834c54993b58a6
d34dr00t <me@ddrt.su>
2016-05-25 10:42:41 -0400

    Initial commit

---
+++ loader.php
@@ -0,0 +1,11 @@
+<?php
+
+ini_set( 'allow_url_fopen', true );
+define( 'FMB_ABS_PATH', dirname( __FILE__ ) . '/' );
+
+require_once FMB_ABS_PATH . 'core/config.php';
+require_once FMB_ABS_PATH . 'core/FacebookHttp.php';
+require_once FMB_ABS_PATH . 'core/functions.php';
+require_once FMB_ABS_PATH . 'core/FacebookMessageReceive.php';
+require_once FMB_ABS_PATH . 'core/FacebookMessageResponse.php';
+require_once FMB_ABS_PATH . 'core/FacebookMessengerBot.php';
